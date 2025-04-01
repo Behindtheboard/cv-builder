f73d9fba1c0c6455f862864cbae6062ab8340eba
Behindtheboard <jk2624@gmail.com>
2025-04-01 14:44:36 -0700

    Add delete function

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -15,10 +15,17 @@ export default function Section({ initSection }) {
     ]);
   }
 
+  function handleDelete(sectionId) {
+    setSections(sections.filter((section) => section.id !== sectionId));
+  }
+
   return (
     <>
       {sections.map((section) => (
-        <SectionInfo initInfo={section.sectInfo} key={section.id} />
+        <>
+          <SectionInfo initInfo={section.sectInfo} key={section.id} />
+          <button onClick={() => handleDelete(section.id)}>Delete</button>
+        </>
       ))}
       <button onClick={handleAdd}>Additional</button>
     </>
