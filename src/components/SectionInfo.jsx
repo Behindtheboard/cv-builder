0df6ff9472e8b4f68c893c6d646ee16910902147
Behindtheboard <jk2624@gmail.com>
2025-04-05 00:05:14 -0700

    Refactor sectionInfo

--- src/components/SectionInfo.jsx
+++ src/components/SectionInfo.jsx
@@ -19,36 +19,30 @@ export default function SectionInfo({ initInfo, onChange }) {
     onChange(infoId, e);
   }
 
-  if (!edit) {
-    return (
-      <>
-        <div className="genSection">
-          {infos.map((info) => (
-            <div key={info.id} className="genInfo">
-              {info.label} {info.value}
-            </div>
-          ))}
-        </div>
-        <button onClick={toggleEdit}>Edit</button>
-      </>
-    );
-  }
-
   return (
     <>
       <form className="genSection">
         {infos.map((info) => (
           <label key={info.id} className="genInput">
             {info.label}
-            <input
-              type={info.type}
-              value={info.value}
-              onChange={(e) => handleInputChange(info.id, e)}
-            />
+
+            {!edit && (
+              <div key={info.id} className="genInfo">
+                {info.value}
+              </div>
+            )}
+
+            {edit && (
+              <input
+                type={info.type}
+                value={info.value}
+                onChange={(e) => handleInputChange(info.id, e)}
+              />
+            )}
           </label>
         ))}
       </form>
-      <button onClick={toggleEdit}>Save</button>
+      <button onClick={toggleEdit}>{(edit) ? 'Save' : 'Edit'}</button>
     </>
   );
 }
